6a5fedc6c87dc428b9b4cfb12ae3c91da3efda1a
Pedro Campos <pedro.campos@startaideia.com.br>
2021-01-20 19:10:40 -0300

    Finished home screen and started details screen

---
+++ covid_19/lib/screens/details_screen.dart
@@ -0,0 +1,37 @@
+import 'package:covid_19/constantes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildDetailsAppBar(context),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+      ),
+    );
+  }
+
+  AppBar buildDetailsAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: kBackgroundColor,
+      elevation: 0,
+      leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back,
+          color: kPrimaryColor,
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset("assets/icons/search.svg"),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
--- covid_19/lib/screens/home_screen.dart
+++ covid_19/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:covid_19/constantes.dart';
+import 'package:covid_19/screens/details_screen.dart';
 import 'package:covid_19/widgets/info_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -8,79 +9,162 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 40),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: kPrimaryColor.withOpacity(0.03),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(50),
-                bottomRight: Radius.circular(50),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding:
+                  EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 40),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: kPrimaryColor.withOpacity(0.03),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                ),
+              ),
+              child: Wrap(
+                runSpacing: 20,
+                spacing: 20,
+                children: <Widget>[
+                  InfoCard(
+                    title: "Confirmed Cases",
+                    iconColor: Color(0xFFFF9C00),
+                    effectedNum: 1062,
+                    press: () {},
+                  ),
+                  InfoCard(
+                    title: "Total Deaths",
+                    iconColor: Color(0xFFFF2055),
+                    effectedNum: 75,
+                    press: () {},
+                  ),
+                  InfoCard(
+                    title: "Total Recovered",
+                    iconColor: Color(0xFF50E3C2),
+                    effectedNum: 689,
+                    press: () {},
+                  ),
+                  InfoCard(
+                    title: "New Cases",
+                    iconColor: Color(0xFF5856D6),
+                    effectedNum: 75,
+                    press: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return DetailsScreen();
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ],
               ),
             ),
-            child: Wrap(
-              runSpacing: 20,
-              spacing: 20,
-              children: <Widget>[
-                InfoCard(
-                  title: "Confirmed Cases",
-                  iconColor: Color(0xFFFF9C00),
-                  effectedNum: 1062,
-                ),
-                InfoCard(
-                  title: "Total Deaths",
-                  iconColor: Color(0xFFFF2055),
-                  effectedNum: 75,
-                ),
-                InfoCard(
-                  title: "Total Recovered",
-                  iconColor: Color(0xFF50E3C2),
-                  effectedNum: 689,
-                ),
-                InfoCard(
-                  title: "New Cases",
-                  iconColor: Color(0xFF5856D6),
-                  effectedNum: 75,
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Preventions",
-                  style: Theme.of(context)
-                      .textTheme
-                      .title
-                      .copyWith(fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    PreventionCard(
-                      svgSrc: "assets/icons/hand_wash.svg",
-                      title: "Wash Hands",
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Preventions",
+                      style: Theme.of(context)
+                          .textTheme
+                          .title
+                          .copyWith(fontWeight: FontWeight.bold),
                     ),
-                    PreventionCard(
-                      svgSrc: "assets/icons/use_mask.svg",
-                      title: "Use Masks",
-                    ),
-                    PreventionCard(
-                      svgSrc: "assets/icons/Clean_Disinfect.svg",
-                      title: "Clean Disinfect",
-                    ),
+                    SizedBox(height: 20),
+                    buildPreventation(),
+                    SizedBox(height: 40),
+                    buildHelpCard(context),
+                    SizedBox(height: 40),
                   ],
                 ),
-              ],
+              ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildPreventation() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        PreventionCard(
+          svgSrc: "assets/icons/hand_wash.svg",
+          title: "Wash Hands",
+        ),
+        PreventionCard(
+          svgSrc: "assets/icons/use_mask.svg",
+          title: "Use Masks",
+        ),
+        PreventionCard(
+          svgSrc: "assets/icons/Clean_Disinfect.svg",
+          title: "Clean Disinfect",
+        ),
+      ],
+    );
+  }
+
+  Container buildHelpCard(BuildContext context) {
+    return Container(
+      height: 150,
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * .4,
+              top: 20,
+              right: 20,
+            ),
+            height: 130,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF60BE93),
+                  Color(0xFF1B8D59),
+                ],
+              ),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Dial 999 for \nMedical Help!",
+                    style: Theme.of(context)
+                        .textTheme
+                        .title
+                        .copyWith(color: Colors.white),
+                  ),
+                  TextSpan(
+                    text: "If any symptoms appear",
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.7),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: SvgPicture.asset("assets/icons/nurse.svg"),
+          ),
+          Positioned(
+            top: 30,
+            right: 10,
+            child: SvgPicture.asset("assets/icons/virus.svg"),
           ),
         ],
       ),
